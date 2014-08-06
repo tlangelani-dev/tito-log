715d6262f554c8fa506200ff66f0da4c9c6ac17c
Tlangelani Mboweni <mbowenit@gmail.com>
2014-08-06 16:22:54 +0200

    update Log.php to properly create logs/default_log.txt relative to the file directory

--- Log.php
+++ Log.php
@@ -3,12 +3,13 @@
 class Log {
 
     // default log file location
-    private $log_file = 'logs/default_log.txt';
+    private $log_file = 'default_log.txt';
 
     public function __construct($user_log_file = null) {
         if ($user_log_file !== null) {
-            $this->log_file = $user_log_file;
+            $this->log_file = __DIR__.'/logs/'.$user_log_file;
         }
+        $this->log_file = __DIR__.'/logs/'.$this->log_file;
         $this->check_file();
     }
 
@@ -21,8 +22,8 @@ class Log {
     // check if file exists, if not create it including directory
     private function check_file() {
         // check log dir
-        if (!file_exists('./logs')) {
-            if (!mkdir('./logs')) {
+        if (!file_exists(__DIR__.'/logs')) {
+            if (!mkdir(__DIR__.'/logs')) {
                 die("<p>Error: Could not create new log file dirextory, please check file permissions.</p>");
             }
         }
